9c8b38759cce95cbc16998432e5fea873caa855f
Stephen Parker <sparker888@gmail.com>
2024-06-26 18:02:26 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["a-us.storyblok.com", "interfacejournal.com"], 
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/banner/wri25HH.js
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import RegisterButton from '@/components/buttons/register-button';
+
+const Banner2025RT = () => {
+  return (
+    <section className="relative w-full" style={{ paddingTop: '33.33%' }}> {/* 33.33% padding for 1500x500 aspect ratio */}
+      <div className="absolute inset-0">
+        <Image
+          src="/kansas-city-skyline.webp"
+          alt="home-banner"
+          layout="fill" // This makes the image cover the entire div
+          className="object-cover w-full h-full" // Tailwind classes for responsiveness and coverage
+          quality={100}
+        />
+      </div>
+      {/* Absolute positioning for text, centered both horizontally and vertically */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="mb-1 text-4xl text-center text-white sm:text-6xl text-shadow-outline">
+            Kansas City, MO
+          </h1>
+          <h1 className="text-4xl text-center text-white sm:text-6xl text-shadow-outline">
+            June 10-12, 2025
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner2025RT;
