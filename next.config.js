c24497af036328532203cf5139c019e9e9865bb9
Stephen Parker <sparker888@gmail.com>
2024-08-28 16:37:15 -0400

    refactored footer into multiple files

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   images: {
     domains: ["a-us.storyblok.com", "interfacejournal.com"], 
   },
-  // Add any other Next.js config options here
+  // Netlify Forms support
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/_next/static/forms/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
