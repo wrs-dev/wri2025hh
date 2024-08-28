f3f66842bd57ea2e1e2efced62772d282cd4303c
Stephen Parker <sparker888@gmail.com>
2024-08-28 16:13:26 -0400

    updated config and toml again for netlify forms

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withNetlify } = require('@netlify/plugin-nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,4 +8,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withNetlify(nextConfig);
