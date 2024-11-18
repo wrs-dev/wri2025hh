5e234f8548631792344ffb8c51501ca21d2fa19a
Stephen Parker <sparker888@gmail.com>
2024-11-18 12:06:58 -0500

    make it just one registration button

--- src/components/registration/index.js
+++ src/components/registration/index.js
@@ -76,16 +76,16 @@ const RegistrationComp = () => {
           <div className="grid grid-cols-1 gap-4 md:grid-cols-1 xl:grid-cols-2">
             {/* Employee Column */}
             <div className="flex flex-col px-4 mb-4">
-            <div className="px-0 lg:px-12">
+              <div className="px-0 lg:px-12">
                 <h3 className="mb-8 text-4xl font-bold text-center text-white">
                   Employee Registration
                 </h3>
                 <p className="pb-4 mb-6 text-white">
-                  Employee Pricing is designed for direct, full time employees
-                  of qualifying railroads, transit agencies and government
+                  Employee Pricing is designed for direct, full-time employees
+                  of qualifying railroads, transit agencies, and government
                   agencies. (You must be paid directly from a railroad, transit
-                  agency or government agency and receive a W-2 or similar). No
-                  Contractors are eligible for Employee Pricing.
+                  agency, or government agency and receive a W-2 or similar). No
+                  contractors are eligible for Employee Pricing.
                 </p>
                 <p className="mb-10 text-white">
                   Employees registering for more than one event will receive
@@ -95,22 +95,19 @@ const RegistrationComp = () => {
               <div className="mt-auto">
                 <PricingEmployeeIndividual />
                 <PricingEmployeeMulti />
-                <div className="mt-12 mb-12 text-center lg:mb-0">
-                  <RegisterButton />
-                </div>
               </div>
             </div>
 
             {/* Professional Column */}
             <div className="flex flex-col px-4 mt-8 mb-4 lg:mt-0">
-            <div className="px-0 lg:px-12">
-            <h3 className="mb-8 text-4xl font-bold text-center text-white">
+              <div className="px-0 lg:px-12">
+                <h3 className="mb-8 text-4xl font-bold text-center text-white">
                   Professional Registration
                 </h3>
                 <p className="pb-4 mb-6 text-white">
                   Professional Pricing is for suppliers, contractors,
-                  consultants and others who are not an employee of a railroad,
-                  transit agency or government agency.
+                  consultants, and others who are not an employee of a railroad,
+                  transit agency, or government agency.
                 </p>
                 <p className="mb-10 text-white">
                   Professionals registering for more than one event will receive
@@ -120,30 +117,31 @@ const RegistrationComp = () => {
               <div className="mt-auto">
                 <PricingProfessionalIndividual />
                 <PricingProfessionalMulti />
-                <div className="mt-12 text-center">
-                  <RegisterButton />
-                </div>
               </div>
             </div>
           </div>
-          
+
+          {/* Centered Register Button below both columns */}
+          <div className="mt-12 text-center">
+            <RegisterButton />
+          </div>
+
           {/* Virtual Pricing centered below the two columns 
           <div className="flex justify-center mt-8">
             <div className="flex flex-col w-full px-4 mb-4 md:w-3/4 xl:w-1/2">
-            <div className="px-0 lg:px-12">
+              <div className="px-0 lg:px-12">
                 <h3 className="mb-8 text-4xl font-bold text-center text-white">
-                Virtual Option
+                  Virtual Option
                 </h3>
                 <p className="pb-4 mb-6 text-center text-white">
-                All attendees are eligible for the following virtual pricing.
+                  All attendees are eligible for the following virtual pricing.
                 </p>
                 <p className="mb-10 text-center text-white">
-                Attendees registering for more than one event will receive special Multi-Event Discounts.
+                  Attendees registering for more than one event will receive special Multi-Event Discounts.
                 </p>
               </div>
               <PricingVirtualIndividual />
               <PricingVirtualMulti />
-              
               <div className="mt-12 mb-12 text-center lg:mb-0">
                 <RegisterButton />
               </div>
