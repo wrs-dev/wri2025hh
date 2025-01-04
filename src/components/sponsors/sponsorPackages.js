30b384d6918493a7814d502e7d1f11d3b3a7f9bc
Stephen Parker <sparker888@gmail.com>
2025-01-03 21:38:07 -0500

    removed extra space

--- src/components/sponsors/sponsorPackages.js
+++ src/components/sponsors/sponsorPackages.js
@@ -379,7 +379,7 @@ export default function SponsorPackages() {
             their information now. If not, you can contact {' '}
                 <a href="mailto:erica@wheel-rail-seminars.com">
                     erica@wheel-rail-seminars.com
-                </a>{' '} to have us add them later .
+                </a>{' '} to have us add them later.
           </p>
         </section>
       </div>
