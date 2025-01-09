acf72f5bd0f1cf26ca7cbb1d2d52fb2366238ec8
Stephen Parker <sparker888@gmail.com>
2025-01-09 14:52:40 -0500

    changed link on sponsor cta 2 on the home page

--- src/components/sponsors/sponsor-cta-2.js
+++ src/components/sponsors/sponsor-cta-2.js
@@ -37,7 +37,7 @@ export default function SponsorCTA2() {
               </p>
               <div className="flex items-center justify-center mt-10 gap-x-6 lg:justify-start">
                 <a
-                  href="https://cvent.me/DeeEKR"
+                  href="/sponsor-packages"
                   className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
                 >
                   Sponsor Registration
