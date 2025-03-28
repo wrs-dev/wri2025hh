0b51878166fefc5ae6f84e5a0cfc987391447fb7
Stephen Parker <sparker888@gmail.com>
2025-03-28 10:47:20 -0500

    updated PC bios & abstracts

--- src/pages/principles-course-bios-abstracts.js
+++ src/pages/principles-course-bios-abstracts.js
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import Head from 'next/head';
 import Link from 'next/link';
 import Navigation from '@/components/navigation';
-import Banner from '@/components/banner';
-import IconLinks from '@/components/icons';
+import Banner25HH from '@/components/banner/wri25HH';
+import IconsHH from '@/components/icons/iconsHH';
 import BiosAbstractsPC from '@/components/bios-abstracts/bios-abstractsPC';
 import Registration from '@/components/registration';
 import SponsorsCloud from '@/components/sponsors';
@@ -12,9 +12,9 @@ import SponsorsCloud from '@/components/sponsors';
 export async function getStaticProps() {
   return {
     props: {
-      title: "WRI 2024 | Principles Course Speaker Bios & Abstracts",
-      description: "Speaker Bios & Abstracts for the 29th Annual Wheel/Rail Interaction Conference (WRI)",
-      socialImage: "/wri2024-social.png" 
+      title: "WRI 2025 | Principles Course Speaker Bios & Abstracts",
+      description: "Speaker Bios & Abstracts for the 30th Annual Wheel/Rail Interaction Conference (WRI)",
+      socialImage: "/wri2025-social.png" 
     }
   };
 }
@@ -23,12 +23,12 @@ const BiosAbstractsPCPage = () => {
   return (
     <main className="bg-white">
       <Navigation />
-      <Banner />
-      <IconLinks />
+      <Banner25HH />
+      <IconsHH />
       <div className="seminar-about-text"></div>
       <BiosAbstractsPC />
       <Registration />
-      <SponsorsCloud />
+      {/*<SponsorsCloud />*/}
     </main>
   );
 };
