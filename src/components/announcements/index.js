b1123148016eeacb8be2d33f22809c766ba79657
Stephen Parker <sparker888@gmail.com>
2025-05-28 14:50:56 -0400

    added link to announcement image

--- src/components/announcements/index.js
+++ src/components/announcements/index.js
@@ -11,14 +11,18 @@ export default function WRIAnnouncements() {
         <h1 className="mb-1 text-2xl font-bold text-wri-dark-blue">
           WRI 2025 Highlighted in RT&amp;S
         </h1>
-        <Image
-          src="/rtands-article.png"
-          alt="WRI 2025 RT&S Article"
-          width={776}
-          height={612}
-          className="mb-4"
-          style={{ width: "100%", height: "auto", maxWidth: "776px" }}
-        />
+        <Link href="https://www.rtands.com/technology/wheel-rail-interface-conference-2025-to-be-held-in-kansas-city-mo/" passHref legacyBehavior>
+          <a target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/rtands-article.png"
+              alt="WRI 2025 RT&S Article"
+              width={776}
+              height={612}
+              className="mb-4"
+              style={{ width: "100%", height: "auto", maxWidth: "776px" }}
+            />
+          </a>
+        </Link>
         <p>
           Railway Track &amp; Structures (RT&amp;S) has published an article announcing
           the upcoming Wheel Rail Interface 2025 Heavy Haul Conference, to be held in Kansas City, Missouri.{" "}
